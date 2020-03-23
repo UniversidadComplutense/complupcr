0a86b3097dbe8de051ed1af63b68e7c420e14a2b
pmarrasant <pmarrasant@ucm.es>
2020-03-23 19:38:28 +0100

    Commit inicial

---
+++ pcr-covid19/src/main/java/es/ucm/pcr/PcrCovid19Application.java
@@ -0,0 +1,13 @@
+package es.ucm.pcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcrCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcrCovid19Application.class, args);
+	}
+
+}
---
+++ pcr-covid19/src/test/java/es/ucm/pcr/PcrCovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ucm.pcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcrCovid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
