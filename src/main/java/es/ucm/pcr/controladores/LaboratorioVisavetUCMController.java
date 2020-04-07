f4c06bd35e31430edf1ac974f3f671cf0f43ab17
yroldan <yroldan@ucm.es>
2020-04-07 16:23:43 +0200

    @Preauthorize

--- src/main/java/es/ucm/pcr/controladores/LaboratorioVisavetUCMController.java
+++ src/main/java/es/ucm/pcr/controladores/LaboratorioVisavetUCMController.java
@@ -78,8 +78,8 @@ public class LaboratorioVisavetUCMController {
 	
 	@Autowired
 	LaboratorioCentroServicio laboratorioCentroServicio;
-  @Autowired
-  LaboratorioVisavetServicio laboratorioVisavetServicio;
+    @Autowired
+    LaboratorioVisavetServicio laboratorioVisavetServicio;
 
 	@SuppressWarnings("unused")
 	private final static Logger log = LoggerFactory.getLogger(LaboratorioVisavetUCMController.class);
@@ -100,18 +100,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 	return busquedaLotes;
 }
 	public ModelAndView buscarLotes(BusquedaLotesBean busquedaLotes, HttpServletRequest request, HttpSession session, Pageable pageable) throws Exception {
-		/* ModelAndView vista = new ModelAndView("VistaListadoRecepcionLotes");
-		// invocar al servicio que dado id De laboratorio se obtiene la entidad laboratorioUni
-		Page<LoteBeanPlacaVisavet> paginaLotes = null;
-		paginaLotes = servicioLaboratorioUni.buscarLotes(busquedaLotes, pageable);
-		List<LoteBeanPlacaVisavet> list= new ArrayList();
-		for (int i = 0; i<10; i++) {
-			list.add(getBean(i));
-		}		
-		paginaLotes = new PageImpl<LoteBeanPlacaVisavet>(list, pageable,20);
-		vista.addObject("busquedaLotes", busquedaLotes);
-		vista.addObject("paginaLotes", paginaLotes);
-		return vista; */
+		
 		Page<LoteBeanPlacaVisavet> paginaLotes = null;
 		ModelAndView vista = new ModelAndView("VistaListadoRecepcionLotes");
 	
@@ -126,6 +115,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 	}
 	// presenta la pagina con unos criterios de busqueda iniciales
 	@RequestMapping(value = "/laboratorioUni/buscarLotes", method = RequestMethod.GET)
+	@PreAuthorize("hasAnyRole('ADMIN','RECEPCIONLABORATORIO','TECNICOLABORATORIO')")
 	public ModelAndView buscarLotesGet(Model model, HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20) Pageable pageable) throws Exception {
          // tengo que mirar como a partir del usuario vemos de que laboratorioUni es y le muestro unicamente sus loooooootes
 		log.info("numero get "+pageable.getPageNumber());
@@ -210,6 +200,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 	// buscar lotes segun los criterios de busqueda 
 	
 		@RequestMapping(value = "/laboratorioUni/buscarLotes", method = RequestMethod.POST)
+		@PreAuthorize("hasAnyRole('ADMIN','RECEPCIONLABORATORIO','TECNICOLABORATORIO')")
 		public ModelAndView buscarLotesPost(@ModelAttribute("busquedaLotes") BusquedaLotesBean busquedaLotes, Model model, HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20,sort = "lote", direction =Sort.Direction.DESC) Pageable pageable) throws Exception {
 	        // tengo que mirar como a partir del usuario vemos de que laboratorioUni es y le muestro unicamente sus loooooootes
 		return  this.buscarLotes(busquedaLotes, request, session, pageable);
@@ -220,6 +211,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 	
 	//@ResponseBody
 	@RequestMapping(value = "/laboratorioUni/confirmarReciboLote", method = RequestMethod.GET)
+	@PreAuthorize("hasAnyRole('ADMIN','RECEPCIONLABORATORIO','TECNICOLABORATORIO')")
 	public ModelAndView confirmarReciboLote(@RequestParam("id") Integer id,Model model, HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20,sort = "lote", direction =Sort.Direction.DESC) Pageable pageable) throws Exception {
 	// llamar al servicio lotes y cambiar el estado de id a Recibido
 		// para probar
@@ -264,6 +256,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 				    return vista;	*/
 	}
 	@RequestMapping(value = "/laboratorioUni/confirmarEnviadaPlaca", method = RequestMethod.GET)
+	@PreAuthorize("hasAnyRole('ADMIN','TECNICOLABORATORIO')")
 	public ModelAndView confirmarEnviadaPlaca(@RequestParam("id") Integer id,Model model, HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20,direction =Sort.Direction.DESC) Pageable pageable) throws Exception {
 		BeanPlacaVisavetUCM placa= servicioLaboratorioUni.buscarPlacaById(id);
 		//BeanLaboratorioCentro laboratorioBean =laboratorioCentroServicio.buscarLaboratorioById(laboratorio);
@@ -277,6 +270,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 	
 		// refrescar  datos con ajax
 		@RequestMapping(value = "/laboratorioUni/actualizarLote", method = RequestMethod.POST)
+		@PreAuthorize("hasAnyRole('ADMIN','TECNICOLABORATORIO')")
 		public String buscarPlacasPost(@RequestParam("id") String id, Model model, HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20) Pageable pageable) {
 			Page<LoteBeanPlacaVisavet> paginaLotes = null;
 			BusquedaLotesBean busquedaLotes=(BusquedaLotesBean) model.getAttribute("busquedaLotes");
@@ -294,6 +288,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 		
 		
 		@RequestMapping(value = "/laboratorioUni/mostrarMuestras", method = RequestMethod.GET)
+		@PreAuthorize("hasAnyRole('ADMIN','RECEPCIONLABORATORIO','TECNICOLABORATORIO')")
 		public String consultarMuestrasLote(@RequestParam("id") Integer id,Model model, HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20,sort = "lote", direction =Sort.Direction.DESC) Pageable pageable) {
 		// ir al servicio lotes y llamar al metodo que me liste las muestra a partir del id de lote
 		// para probar
@@ -328,6 +323,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 		
 		//este metodo obtiene los lotes q estan listos para ser procesados en placas visavet y muestran en las placas
 		@RequestMapping(value = "/laboratorioUni/procesarLotes", method = RequestMethod.GET)
+		@PreAuthorize("hasAnyRole('ADMIN','TECNICOLABORATORIO')")
 		public ModelAndView buscarPlacasGet(@RequestParam("lotes") String lotes,Model model, HttpServletRequest request, HttpSession session) {
 			ModelAndView vista = new ModelAndView("VistaLotesPlacasVisavet");
 			LotePlacaVisavetBean lotePlacaVisavetBean= new LotePlacaVisavetBean();
@@ -370,6 +366,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 		}
 		@ResponseBody
 		@RequestMapping(value = "/laboratorioUni/altaPlacaVisavet", method = RequestMethod.GET)
+		@PreAuthorize("hasAnyRole('ADMIN','TECNICOLABORATORIO')")
 		public Integer altaPlacasGet(Model model, HttpServletRequest request, HttpSession session) {
 			// para obtener un numero de Placa voy a crear una placa vacia
 			// la creo vacia pq quiero obtener solo el num de placa
@@ -468,6 +465,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 		    return vista;
 		}
 		@RequestMapping(value = "/laboratorioUni/buscarPlacas", method = RequestMethod.POST)
+		@PreAuthorize("hasAnyRole('ADMIN','TECNICOLABORATORIO')")
 		public ModelAndView buscarPlacasPost(@ModelAttribute("busquedaPlacasVisavetBean") BusquedaPlacasVisavetBean busquedaPlacasVisavetBean, Model model, HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20) Pageable pageable) throws Exception {
 	
 			return  this.buscarPlacas(busquedaPlacasVisavetBean, request, session, pageable);
@@ -561,6 +559,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 		}
 			
 		@RequestMapping(value = "/laboratorioUni/consultarOcupacionLaboratorios", method = RequestMethod.GET)
+		@PreAuthorize("hasAnyRole('ADMIN','TECNICOLABORATORIO')")
 	   public String asignarPlacasGet(@RequestParam("idPlaca") Integer idPlaca, Model model, HttpServletRequest request, HttpSession session) throws Exception {
 		//	
 			List<BeanLaboratorioCentro> laboratorios=laboratorioCentroServicio.listaLaboratoriosCentroOrdenada();
@@ -573,6 +572,7 @@ private  BusquedaLotesBean rellenarBusquedaLotes(BusquedaLotesBean busquedaLotes
 		
 		
 		@RequestMapping(value="/laboratorioUni/asignarLaboratorio", method=RequestMethod.GET)
+		@PreAuthorize("hasAnyRole('ADMIN','TECNICOLABORATORIO')")
 		public ModelAndView asignarPlacasPost(@RequestParam("idPlaca") Integer idPlaca, @RequestParam("laboratorio") Integer laboratorio,Model model,HttpServletRequest request, HttpSession session,@PageableDefault(page = 0, value = 20) Pageable pageable) throws Exception {
 			//PlacaLaboratorioVisavetBean  placa=laboratorioVisavetServicio.buscarPlaca(idPlaca);
 			BeanPlacaVisavetUCM placa= servicioLaboratorioUni.buscarPlacaById(idPlaca);
