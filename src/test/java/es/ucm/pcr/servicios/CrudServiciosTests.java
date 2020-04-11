562bf90cf0324c6035b7d9af990b4567e9b448f5
pmarrasant <pmarrasant@ucm.es>
2020-04-11 23:17:54 +0200

    Añadida infraestructura de TescContainer con mysql docker para tests

---
+++ src/test/java/es/ucm/pcr/servicios/CrudServiciosTests.java
@@ -0,0 +1,33 @@
+package es.ucm.pcr.servicios;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import es.ucm.pcr.modelo.orm.Centro;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles(profiles = "test")
+@SpringBootTest
+public class CrudServiciosTests {
+	
+	@Autowired
+	CentroServicio centroServicio;
+	
+	@Test
+	public void getCentro() throws Exception {
+		Optional<Centro> centroop = centroServicio.buscarCentroPorId(1);
+		Centro centro = centroop.get();
+		assertEquals("El código de centro coincide","BOT",centro.getCodCentro());
+		
+	}
+	
+	
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,9 @@
+
+--
+-- Dumping data for table `centro`
+--
+INSERT INTO centro (id, nombre, codCentro, telefono, email, direccion) VALUES 
+	(1,'Jardín Botánico','BOT','123454321','botanicPrueba@ucm.es',NULL)
+	,(2,'Prueba','PRU','7989678','lkjlkh@hhfs.es',NULL)
+	,(3,'Pruebas_Tere modificado','PRT','123456789','centro@ucm.es','calle maja')
+	,(4,'Centro de demostración','DEMO','913944925','nnnn@njj.es','kjbkh');
