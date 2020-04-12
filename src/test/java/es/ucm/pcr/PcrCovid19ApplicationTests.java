0e8fa9f32ad1aa6c3f1948c26f8e15131daddedb
pmarrasant <pmarrasant@ucm.es>
2020-04-12 21:42:33 +0200

    Cambios en tests de integración

--- src/test/java/es/ucm/pcr/PcrCovid19ApplicationTests.java
+++ src/test/java/es/ucm/pcr/PcrCovid19ApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles(profiles = "test")
-//@ActiveProfiles(profiles = "integracion")
 @SpringBootTest
 public class PcrCovid19ApplicationTests {
 
